a4a735c041924c95db80864f56f4f859ae5bedd5
Raphael Croce <rcroce7@gmail.com>
2018-11-12 01:23:15 -0600

    Implement post controller methods to create and show new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,13 +5,28 @@ class PostsController < ApplicationController
 
   def new
     # Return view to create a new Post
+    @post = Post.new
   end
 
   def create
     # Add a new `Post` to the database
+    # render plain: params[:post].inspect
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def destroy
     # Remove a `Post` from the database
+  end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
