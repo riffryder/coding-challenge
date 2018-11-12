9f664b3bbf4d3116484582b19fb95b04be82c76b
Raphael Croce <rcroce7@gmail.com>
2018-11-12 13:45:12 -0600

    Add comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
+  #establish 1:1 association to post table
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
+  #establish 1:M association with comments and establish depedency for post deletion
   has_many :comments, dependent: :destroy
 
+  # search method to look through blog title and body
   def self.search(search)
     if search
       where('title LIKE ? OR body LIKE ?', "%#{search}%", "%#{search}%")
